1d5bcd744ec5e25bc2d8a4f4e6a25909a363ab52
Robert Riebisch <15858666+bttrx@users.noreply.github.com>
2025-10-05 20:19:33 +0200

    Update spoiler.php
    Reflect API change in Markdown 0.9.4

--- spoiler.php
+++ spoiler.php
@@ -2,7 +2,7 @@
 // Spoiler extension, https://github.com/schulle4u/yellow-extensions-schulle4u/tree/main/spoiler
 
 class YellowSpoiler {
-    const VERSION = "0.9.1";
+    const VERSION = "0.9.2";
     public $yellow;            //access to API
     
     // Handle initialisation
@@ -13,7 +13,7 @@ class YellowSpoiler {
     // Handle page content element
     public function onParseContentElement($page, $name, $text, $attributes, $type) {
         $output = null;
-        if ($name=="spoiler" && $type=="notice") {
+        if ($name=="spoiler" && $type=="general") {
             $htmlAttributes = $this->yellow->lookup->getHtmlAttributes($attributes);
             $output = "<details$htmlAttributes>\n";
             $output .= "<summary>".htmlspecialchars(ucfirst($name))."</summary>\n";
